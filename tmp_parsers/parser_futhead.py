29dee182d758488d0d1a30eb060a2c892459719b
boskorban <HakHwc7SJ77c4gHw>
2020-01-07 10:06:23 +0100

    futhead parser connection/inserting data to sqlite3 db

--- tmp_parsers/parser_futhead.py
+++ tmp_parsers/parser_futhead.py
@@ -3,6 +3,8 @@ import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 import unicodedata
+import sqlite3
+from sqlite3 import Error
 
 # List Intializations
 players = []
@@ -13,7 +15,7 @@ FutHead = requests.get('https://www.futhead.com/20/players/?level=all_nif&bin_pl
 bs = BeautifulSoup(FutHead.text, 'html.parser')
 TotalPages = int(re.sub('\s +', '', str(bs.find('span', {'class': 'font-12 font-bold margin-l-r-10'}).get_text())).split(' ')[1])
 print('Number of pages to be parsed for FIFA players: ' + str(TotalPages))
-for page in range(1, TotalPages + 1):
+for page in range(1,  TotalPages + 1):
     FutHead = requests.get('https://www.futhead.com/20/players/?level=all_nif&page={}&bin_platform=pc'.format(str(page)))
     bs = BeautifulSoup(FutHead.text, 'html.parser')
     Stats = bs.findAll('span', {'class': 'player-stat stream-col-60 hidden-md hidden-sm'})
@@ -27,7 +29,7 @@ for page in range(1, TotalPages + 1):
     for i in range(num):
         p = []
         tmp_name = Names[i].get_text().upper()
-        tmp_name = tmp_name.replace("...", "")
+        tmp_name = tmp_name.replace("...", "").replace("'", " ")
         tmp_name = unicodedata.normalize('NFD', tmp_name).encode('ascii', 'ignore').decode('utf8')
         p.append(tmp_name)
         strong = Information[i].strong.extract()
@@ -57,10 +59,44 @@ for page in range(1, TotalPages + 1):
     print('Page ' + str(page) + ' is done!')
 
 # Inserting data into its specific table
+#tmp = 'NAME;CLUB;LEAGUE;POSITION;RATING;PACE;SHOOTING;PASSING;DRIBBLING;DEFENDING;PHYSICAL\n'
+#for player, attribute in zip(players, attributes):
+#    tmp = tmp + "{};{};{};{};{};{};{};{};{};{};{}\n".format(player[0], player[1], player[2], player[3], player[4], attribute[0], attribute[1], attribute[2], attribute[3], attribute[4], attribute[5])
 
-tmp = 'NAME;CLUB;LEAGUE;POSITION;RATING;PACE;SHOOTING;PASSING;DRIBBLING;DEFENDING;PHYSICAL\n'
-for player, attribute in zip(players, attributes):
-    tmp = tmp + "{};{};{};{};{};{};{};{};{};{};{}\n".format(player[0], player[1], player[2], player[3], player[4], attribute[0], attribute[1], attribute[2], attribute[3], attribute[4], attribute[5])
+#with open("output.csv", "w") as text_file:
+#    text_file.write(tmp)
 
-with open("output.csv", "w") as text_file:
-    text_file.write(tmp)
+sql_create_table = """CREATE TABLE IF NOT EXISTS players (
+                                    id integer PRIMARY KEY,
+                                    name text NOT NULL,
+                                    club text,
+                                    league text,
+                                    position text,
+                                    rating integer NOT NULL,
+                                    pace integer NOT NULL,
+                                    shooting integer NOT NULL,
+                                    passing integer NOT NULL,
+                                    dribbling integer NOT NULL,
+                                    defending integer NOT NULL,
+                                    physical integer NOT NULL
+                                );"""
+
+
+conn = None
+try:
+    conn = sqlite3.connect('players_futhead.db')
+
+    if conn:
+        c = conn.cursor()
+        c.execute(sql_create_table)
+
+        for player, attribute in zip(players, attributes):
+            sql = "INSERT INTO players (name, club, league, position, rating, pace, shooting, passing, dribbling, defending, physical) VALUES ('{}','{}','{}','{}',{},{},{},{},{},{},{})".format(player[0], player[1], player[2], player[3], player[4], attribute[0], attribute[1], attribute[2], attribute[3], attribute[4], attribute[5])
+            cur = conn.cursor()
+            cur.execute(sql)
+        conn.commit()
+except Error as e:
+    print(e)
+finally:
+    if conn:
+        conn.close()
